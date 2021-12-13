e14713ef29cec9d6858e495eed91faac5abc0b97
jocex <jjx2005@nyu.edu>
2021-12-13 15:12:10 -0500

    Broken Platform Animation

--- Assets/Scripts/Broken.cs
+++ Assets/Scripts/Broken.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class Broken : MonoBehaviour
 {
     public EdgeCollider2D cc;
+    public Sprite BrokenSprite;
+    
 
      private void Awake(){
         cc=GetComponent<EdgeCollider2D>();
         cc.isTrigger = true;
     }
 
+    void Start(){
+        //spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
     
     
     private void OnTriggerEnter2D(Collider2D other){
         if( other.CompareTag("Character")){
-             gameObject.SetActive(false);
-            
+           gameObject.GetComponent<SpriteRenderer>().sprite = BrokenSprite;
+             //gameObject.SetActive(false);
+          GetComponent<Rigidbody2D>().gravityScale = 1f;
+         //  GetComponent<Rigidbody2D>().isStatic = false;
+           Destroy (gameObject, 3);
            
            
         }
